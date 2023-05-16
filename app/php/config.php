b51ae817cfd425faa3a0dfb8dffd25a9f5d09233
root <root@ProfessionalSkillsWebMQTT.tailf8ae8.ts.net>
2023-05-16 16:48:34 +0000

    Added MQTT to config

--- app/php/config.php
+++ app/php/config.php
@@ -1,8 +1,15 @@
 <?php
+// MySQL database Configuration
 $DB_SERVER = "localhost";
 $DB_USERNAME = "boekenzoeker";
 $DB_PASSWORD = "boekenzoeker";
 $DB_DATABASE = "boekenzoeker";
+
+// MQTT Configuration
+$MQTT_SERVER = "10.0.30.40";
+$MQTT_PORT = 1883;
+
+
 
 // Create connection
 $conn = new mysqli($DB_SERVER, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE);
